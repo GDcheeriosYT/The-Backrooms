ae96c923fc67e4792e50df27cb0917bec1bbc7cd
GDcheerios <minecreeper0913@gmail.com>
2022-01-18 16:06:17 -0600

    disabled Vsync

--- main.py
+++ main.py
@@ -85,5 +85,6 @@ window.borderless = False               # Show a border
 window.fullscreen = False                # Do not go Fullscreen
 window.exit_button.visible = False      # Do not show the in-game red X that loses the window
 window.fps_counter.enabled = True       # Show the FPS (Frames per second) counter
+window.vsync = False
 
 app.run()
