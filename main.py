8002f32244b68439e1e825747c978e1718cd9b12
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2022-01-19 10:58:33 -0600

    moved window properties

--- main.py
+++ main.py
@@ -37,13 +37,6 @@ from game import buttons
 
 app = Ursina()
 
-#window setup
-window.title = 'The Backrooms'          # The window title
-window.borderless = False               # Show a border
-window.fullscreen = False                # Do not go Fullscreen
-window.exit_button.visible = False      # Do not show the in-game red X that loses the window
-window.fps_counter.enabled = True       # Show the FPS (Frames per second) counter
-window.vsync = False
 
 #player
 player = br_player.player("GDcheerios", y=5)
