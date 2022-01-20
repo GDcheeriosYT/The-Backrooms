74310db3697dcb3a0818f502592ba5fe1fb11e88
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2022-01-20 15:19:00 -0600

    moved app setup

--- main.py
+++ main.py
@@ -39,6 +39,17 @@ random.seed(input("seed: "))
 
 app = Ursina()
 
+#window setup
+window.title = 'The Backrooms'          # The window title
+window.borderless = False               # Show a border
+window.fullscreen = False                # Do not go Fullscreen
+window.exit_button.visible = False      # Do not show the in-game red X that loses the window
+window.fps_counter.enabled = True       # Show the FPS (Frames per second) counter
+window.vsync = False
+
+#texture setup
+Texture.default_filtering = "mipmap"
+
 
 #player
 player = br_player.player("GDcheerios")
