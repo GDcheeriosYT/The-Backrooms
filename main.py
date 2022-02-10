26f0ce1b7899d5725899fe98a5791264618ac409
GDcheerios <minecreeper0913@gmail.com>
2022-02-10 17:33:11 -0600

    made fullscreen

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ parent_light_entity = Entity()
 #window setup
 window.title = 'The Backrooms'          # The window title
 window.borderless = False               # Show a border
-window.fullscreen = False                # Do not go Fullscreen
+window.fullscreen = True                # Do not go Fullscreen
 window.exit_button.visible = False      # Do not show the in-game red X that loses the window
 window.fps_counter.enabled = True       # Show the FPS (Frames per second) counter
 window.vsync = False
