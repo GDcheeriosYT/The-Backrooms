ae5b1216ce26dbbd86e9871c8c0d342f8b37e22a
GDcheerios <minecreeper0913@gmail.com>
2022-02-11 00:39:02 -0600

    added health, hydration, hunger, sanity, and items to player class

--- game/player.py
+++ game/player.py
@@ -1,10 +1,17 @@
+from multiprocessing.sharedctypes import Value
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.prefabs.health_bar import HealthBar
 
 class Player:
-  def __init__(self, name, color):
+  def __init__(self, name, color, health=100, hydration=100, hunger=100, sanity=100, items={}):
     self.name = name
     self.color = color
+    self.health = health
+    self.hydration = hydration
+    self.hunger = hunger
+    self.sanity = sanity
+    self.items = {}
   
   def change_color(self, new_color):
     self.color = new_color
@@ -34,4 +41,29 @@ class Player:
                            x=-2.5,
                            color=rgb(self.color[0],self.color[1],self.color[2]))
     
+    self.health_bar = HealthBar(value=self.health)
+    
     camera.position = (0,10,0)
+  
+  def damage(self, damage):
+    self.health -= damage
+    self.health_bar.value = self.health
+    if self.health <= 0:
+      self.controller.position = (0, 0, 0)
+      self.health = 100
+  
+  def thirst(self, thirst):
+    self.hydration -= thirst
+  
+  def craze(self, craze):
+    self.sanity -= craze
+    
+  def heal(self, heal):
+    self.health += heal
+    self.health_bar.value = self.health
+  
+  def hydrate(self, liquid):
+    self.hydration += liquid
+  
+  def uninsane(self, vbucks):
+    self.sanity += vbucks
