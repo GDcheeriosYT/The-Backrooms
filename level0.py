dd99330a66c461cbdf8713f3ccc8ad40136bf543
GDcheerios <minecreeper0913@gmail.com>
2022-02-21 01:18:04 -0600

    added way to place door

--- level0.py
+++ level0.py
@@ -1,7 +1,12 @@
 from ursina import *
 from UrsinaLighting import *
 
+#initial variable stuff
+wall_side_scale = 0.5
+
 #initial map stuff
+door = Entity()
+collider = Entity(collider="box")
 wall = LitObject(model="cube",
               texture=Texture("resources/levels/level 0/wall.png"),
               scale=(0,0,0),
@@ -25,3 +30,34 @@ ceiling = LitObject(model="cube",
                  position=(0,6,0),
                  specularMap=load_texture("resources/levels/level 0/noreflect.png"),
                  cubemapIntensity=0)
+
+def place_door(x=0, y=0, z=0, sideways=False):
+  change = 2
+  if sideways == False:
+    duplicate(wall, position=(x-change, y, z), scale=(1.5, 20, wall_side_scale), parent=door)#left side of door
+    duplicate(wall, position=(x+change, y, z), scale=(1.5, 20, wall_side_scale), parent=door)#right side of door
+    duplicate(wall, position=(x, y+5, z), scale=(5, 2, wall_side_scale), parent=door)#top side of door
+    duplicate(collider, position=(x-change, y, z), scale=(1,20,wall_side_scale))
+    duplicate(collider, position=(x+change, y, z), scale=(1,20,wall_side_scale))
+  else:
+    duplicate(wall, position=(x, y, z-change), scale=(wall_side_scale, 20, 1.5), parent=door)#left side of door
+    duplicate(wall, position=(x, y, z+change), scale=(wall_side_scale, 20, 1.5), parent=door)#right side of door
+    duplicate(wall, position=(x, y+5, z), scale=(wall_side_scale, 2, 5), parent=door)#top side of door
+    duplicate(collider, position=(x, y, z-change), scale=(wall_side_scale,20,1))
+    duplicate(collider, position=(x, y, z+change), scale=(wall_side_scale,20,1))
+
+
+
+
+#class stuff
+class Chunk():
+  def __init__(self, x=0, y=0, z=0, walls=1, has_door=False):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.walls = walls
+    self.has_door = has_door
+    
+  def place(self):
+    for wall in range(walls):
+      None
