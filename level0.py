90a28ffe0bf9da2ca82dc943ddb8cb8f75e099dc
GDcheerios <minecreeper0913@gmail.com>
2022-02-21 03:20:03 -0600

    changes position variables

--- level0.py
+++ level0.py
@@ -32,32 +32,71 @@ ceiling = LitObject(model="cube",
                  position=(0,6,0),
                  specularMap=load_texture("resources/levels/level 0/noreflect.png"),
                  cubemapIntensity=0)
+parent_light_entity = Entity()
+light = Entity(model="cube", texture=Texture("resources/levels/level 0/light.png"), color=color.white, position=(100,5.8,100), scale=(2.2,1.2,4), specularMap=load_texture("resources/levels/level 0/noreflect.png"))
+LitPointLight(position=Vec3(0,0,0), intensity=1, color=rgb(248, 252, 150))
 
 def place_door(x=0, y=0, z=0, sideways=False):
-  change = 2
   if sideways == False:
-    duplicate(wall, position=(x-change, y, z), scale=(1.5, 20, wall_side_scale), parent=door)#left side of door
-    duplicate(wall, position=(x+change, y, z), scale=(1.5, 20, wall_side_scale), parent=door)#right side of door
-    duplicate(wall, position=(x, y+5, z), scale=(5, 2, wall_side_scale), parent=door)#top side of door
-    duplicate(collider, position=(x-change, y, z), scale=(1,20,wall_side_scale))
-    duplicate(collider, position=(x+change, y, z), scale=(1,20,wall_side_scale))
+    duplicate(wall, position=(x-wall_spacing / 1.5, y, z), scale=(wall_spacing / 1.5, 8, wall_side_scale), parent=door)#left side of door
+    duplicate(wall, position=(x+wall_spacing / 1.5, y, z), scale=(wall_spacing / 1.5, 8, wall_side_scale), parent=door)#right side of door
+    duplicate(wall, position=(x, y+5, z), scale=(wall_spacing * 2, 2, wall_side_scale), parent=door)#top side of door
+    duplicate(collider, position=(x-wall_spacing / 1.5, y, z), scale=(wall_spacing / 1.5,8,wall_side_scale))  
+    duplicate(collider, position=(x+wall_spacing / 1.5, y, z), scale=(wall_spacing / 1.5,8,wall_side_scale))
   else:
-    duplicate(wall, position=(x, y, z-change), scale=(wall_side_scale, 20, 1.5), parent=door)#left side of door
-    duplicate(wall, position=(x, y, z+change), scale=(wall_side_scale, 20, 1.5), parent=door)#right side of door
-    duplicate(wall, position=(x, y+5, z), scale=(wall_side_scale, 2, 5), parent=door)#top side of door
-    duplicate(collider, position=(x, y, z-change), scale=(wall_side_scale,20,1))
-    duplicate(collider, position=(x, y, z+change), scale=(wall_side_scale,20,1))
+    duplicate(wall, position=(x, y, z-wall_spacing / 1.5), scale=(wall_side_scale, 8, wall_spacing / 1.5), parent=door)#left side of door
+    duplicate(wall, position=(x, y, z+wall_spacing / 1.5), scale=(wall_side_scale, 8, wall_spacing / 1.5), parent=door)#right side of door
+    duplicate(wall, position=(x, y+5, z), scale=(wall_side_scale, 2, wall_spacing * 2), parent=door)#top side of door
+    duplicate(collider, position=(x, y, z-wall_spacing / 1.5), scale=(wall_side_scale,20,wall_spacing / 1.5))
+    duplicate(collider, position=(x, y, z+wall_spacing / 1.5), scale=(wall_side_scale,20,wall_spacing / 1.5))
 
 
 
 
 def place_wall(x=0, y=0, z=0, sideways=False):
   if sideways == False:
-    duplicate(wall, position=(x, y, z), scale=(wall_spacing, 20, wall_side_scale), parent=walls)
-    duplicate(collider, position=(x, y, z), scale=(wall_spacing,20,wall_side_scale))
+    duplicate(wall, position=(x, y, z), scale=(wall_spacing * 2, 20, wall_side_scale), parent=walls)
+    duplicate(collider, position=(x, y, z), scale=(wall_spacing * 2, 20,wall_side_scale))
   else:
-    duplicate(wall, position=(x, y, z), scale=(wall_side_scale, 20, wall_spacing), parent=walls)
-    duplicate(collider, position=(x, y, z), scale=(wall_side_scale,20,wall_spacing))
+    duplicate(wall, position=(x, y, z), scale=(wall_side_scale, 20, wall_spacing * 2), parent=walls)
+    duplicate(collider, position=(x, y, z), scale=(wall_side_scale, 20,wall_spacing * 2))
+
+
+
+
+def placement_manager(position, locations):
+  door_placed = False
+  def door_place_check():
+    nonlocal door_placed
+    if door_placed == False:
+      if random.randint(0, 1) == 1:
+        door_placed = True
+        return True
+      else:
+        return False
+    else:
+      return False
+  for location in locations:
+    if location == "left":
+      if door_place_check() == True:
+        place_door(position[0], position[1], position[2] - wall_spacing, True)
+      else:
+        place_wall(position[0], position[1], position[2] - wall_spacing, True)
+    elif location == "top":
+      if door_place_check() == True:
+        place_door(position[0] + wall_spacing, position[1], position[2], False)
+      else:
+        place_wall(position[0] + wall_spacing, position[1], position[2], False)
+    elif location == "right":
+      if door_place_check() == True:
+        place_door(position[0], position[1], position[2] + wall_spacing, True)
+      else:
+        place_wall(position[0], position[1], position[2] + wall_spacing, True)
+    elif location == "bottom":
+      if door_place_check() == True:
+        place_door(position[0] - wall_spacing, position[1], position[2], False)
+      else:
+        place_wall(position[0] - wall_spacing, position[1], position[2], False)
 
 
 
@@ -70,5 +109,17 @@ class Chunk():
     self.walls = walls
     
   def place(self):
-    for wall in range(walls):
-      None
+    duplicate(light,
+                position=(self.x - wall_spacing, 5.8, self.z - wall_spacing),
+                parent=parent_light_entity)
+    LitPointLight(position=Vec3(self.x - wall_spacing, 4, self.z - wall_spacing), color=rgb(248, 252, 150))
+    placement_options = ["left, top, right, bottom"] #in the future add a loop to put these in the locations for more diversity
+    if self.walls == 4:
+      placement_manager((self.x, self.y, self.z), ["left, top, right, bottom"])
+    elif self.walls == 3:
+      placement_manager((self.x, self.y, self.z), ["left", "top", "right"])
+    elif self.walls == 2:
+      placement_manager((self.x, self.y, self.z), ["left", "top"])
+    elif self.walls == 1:
+      placement_manager((self.x, self.y, self.z), ["left"])
+      
