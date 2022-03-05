05fd466aec5a11dc80513213fff5c43020f62c71
GDcheerios <minecreeper0913@gmail.com>
2022-03-05 15:42:56 -0600

    updated lobby

--- lobby.py
+++ lobby.py
@@ -1,33 +1,31 @@
 from ursina import *
 from UrsinaLighting import *
 
-tile = LitObject(model="cube",
-                 texture=Texture('resources/levels/lobby/floor_tile.png'),
-                 collider="box",
-                 smoothness=200,
-                 position=(0, -10, 0),
-                 tiling=(70, 70),
-                 scale=(100, 2, 100))
+#audio
+water_ambiance = Audio("resources/levels/lobby/water_ambiance.mp3", loop=True)
+water_ambiance.play()
 
-wall = LitObject(model="cube",
-                 texture=Texture('resources/levels/lobby/pool_tile.png'),
-                 collider="box",
-                 smoothness=200,
-                 position=(0, -10, 10),
-                 tiling=(30, 150),
-                 scale=(100, 200, 5))
+#variables
+light_range = 100
+light_intensity = 0.2
 
-ceiling = LitObject(model="cube",
-                 texture=Texture('resources/levels/lobby/pool_tile.png'),
-                 collider="box",
-                 smoothness=200,
-                 position=(0, 1, 0),
-                 tiling=(70, 70),
-                 scale=(100, 1, 100))
 
-ceiling_light = LitObject(model="sphere",
-                          color=Vec4(10, 10, 10, 255),
-                          position=(0, 0.7, 0),
-                          scale=(1, 1, 1))
+#main room
+floor = LitObject(model="cube", texture=Texture('resources/levels/lobby/floor_tile.png'), collider="box", smoothness=200, position=(0, -6, 0), tiling=(35, 35), scale=(40, 2, 40))
+ceiling = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), collider="box", smoothness=200, position=(0, 1, 0), tiling=(70, 70), scale=(40, 1, 40))
+wall = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), collider="box", smoothness=200, position=(0, -10, 20), tiling=(10, 6), scale=(40, 30, 1))
+wall = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), collider="box", smoothness=200, position=(0, -10, -20), tiling=(10, 6), scale=(40, 30, 1))
+wall = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), smoothness=200, position=(20, -10, 0), tiling=(6, 10), scale=(1, 30, 40))
+wall = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), collider="box", smoothness=200, position=(-20, -10, 15), tiling=(6, 6), scale=(1, 30, 20))
+wall = LitObject(model="cube", texture=Texture('resources/levels/lobby/pool_tile.png'), collider="box", smoothness=200, position=(-20, -10, -15), tiling=(6, 6), scale=(1, 30, 20))
 
-LitPointLight(position=Vec3(0, -0.6, 0), intensity=0.4, range=100, color=Vec3(1,1,1.3))
+ceiling_light = LitObject(model="sphere", color=Vec4(10, 10, 10, 255), position=(10, 0.7, 0), scale=(1, 1, 1))
+ceiling_light = LitObject(model="sphere", color=Vec4(10, 10, 10, 255), position=(-10, 0.7, 0), scale=(1, 1, 1))
+ceiling_light = LitObject(model="sphere", color=Vec4(10, 10, 10, 255), position=(0, 0.7, 10), scale=(1, 1, 1))
+ceiling_light = LitObject(model="sphere", color=Vec4(10, 10, 10, 255), position=(0, 0.7, -10), scale=(1, 1, 1))
+
+#lighting
+LitPointLight(position=Vec3(10, -0.6, 0), intensity=light_intensity, range=light_range, color=Vec3(0,0,0.4))
+LitPointLight(position=Vec3(-10, -0.6, 0), intensity=light_intensity, range=light_range, color=Vec3(0,0,0.4))
+LitPointLight(position=Vec3(0, -0.6, 10), intensity=light_intensity, range=light_range, color=Vec3(0,0,0.4))
+LitPointLight(position=Vec3(0, -0.6, -10), intensity=light_intensity, range=light_range, color=Vec3(0,0,0.4))
