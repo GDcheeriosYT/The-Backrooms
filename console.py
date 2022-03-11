ec18eca9127fd3e0bd57871daa9eb4917345e5cf
GDcheerios <minecreeper0913@gmail.com>
2022-03-10 22:39:51 -0600

    added a open/closed state

--- console.py
+++ console.py
@@ -5,18 +5,21 @@ from player import *
 from items import *
 
 class Console:
-  def __init__(self):
+  def __init__(self, open=False):
     self.console = Entity(parent="camera.ui")
     self.console_background = Entity(model="cube", color=rgb(30, 180, 30, 200), parent=self.console)
     self.console_output_area = Entity(model="cube", color=rgb(0, 0, 0), parent=self.console)
     self.console_output = []
+    self.open = open
     
   
   def appear(self):
     self.console.enable()
+    self.open = True
   
   def disappear(self):
     self.console.disable()
+    self.open = False
      
   def test_output(self, text):
     self.console_output.append(text)
