0e7fed1fc0f0b997cc54531bd100fb6d0cab165f
GDcheerios <minecreeper0913@gmail.com>
2022-03-11 00:56:02 -0600

    implemented console design

--- console.py
+++ console.py
@@ -6,10 +6,11 @@ from items import *
 
 class Console:
   def __init__(self, open=False):
-    self.console = Entity(parent="camera.ui")
-    self.console_background = Entity(model="cube", color=rgb(30, 180, 30, 200), parent=self.console)
-    self.console_output_area = Entity(model="cube", color=rgb(0, 0, 0), parent=self.console)
+    self.console = Entity(parent=camera.ui, position=(0, 0.25), scale=(2, 0.5))
+    self.console_background = Entity(model="cube", color=rgb(80, 80, 80, 120), parent=self.console)
+    self.console_output_area = Entity(model="cube", color=rgb(0, 0, 0), position=(0, 0.2), scale=(0.85, 0.7), parent=self.console)
     self.console_output = []
+    self.console_input_area = Entity(model="cube", color=rgb(0, 0, 0), position=(0, -0.36), scale=(0.85, 0.2), parent=self.console)
     self.open = open
     
   
